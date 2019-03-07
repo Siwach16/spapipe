6866d51c6052587826f0a7f81b612d5d1f375954
Ajay Siwach <ajaysiwach16@gmail.com>
2019-03-07 21:39:35 +0530

    Update configureConfig.groovy

--- vars/configureConfig.groovy
+++ vars/configureConfig.groovy
@@ -2,10 +2,10 @@ import org.yaml.snakeyaml.Yaml
 import static groovy.json.JsonOutput.prettyPrint
 import static groovy.json.JsonOutput.toJson
 
-def call() {
+def call(jconfig) {
   stage("addjenkins"){
     Yaml parser = new Yaml()
-    def jconfig=parser.load("""
+    jconfig=parser.load("""
         build:
             name: Run Build
             commands: npm install
